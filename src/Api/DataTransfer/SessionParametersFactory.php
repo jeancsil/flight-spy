1f89b72f31b1ae2e354de712f3ea64893aaafe38
Jeancsil <jeancsil@gmail.com>
2016-09-25 21:03:56 +0200

    Add the possibility to create multiple SessionParameters instances based on an array.

--- src/Api/DataTransfer/SessionParametersFactory.php
+++ src/Api/DataTransfer/SessionParametersFactory.php
@@ -8,8 +8,22 @@ namespace Jeancsil\FlightSpy\Api\DataTransfer;
 use Jeancsil\FlightSpy\Entity\Parameter;
 use Symfony\Component\Console\Input\InputInterface;
 
-class SessionParametersFactory {
+class SessionParametersFactory
+{
+    /**
+     * @var string
+     */
     private $apiKey;
+
+    /**
+     * @var array
+     */
+    private $configCache;
+
+    /**
+     * @var array
+     */
+    private $maxPrices;
 
     /**
      * @param string $apiKey
@@ -18,6 +32,29 @@ class SessionParametersFactory {
         $this->apiKey = $apiKey;
     }
 
+    /**
+     * @param string $configFile
+     * @return Parameter[]
+     */
+    public function createFromConfigFile($configFile) {
+        $configurations = json_decode(file_get_contents($configFile), true);
+
+        $parameters = [];
+        $maxPrices = [];
+        foreach ($configurations as $configuration) {
+            $parameters[] = $this->createFromArray($configuration);
+            $maxPrices[] = $this->getValue(Parameter::MAX_PRICE);
+        }
+
+        $this->maxPrices = $maxPrices;
+
+        return $parameters;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @return SessionParameters
+     */
     public function createFromInput(InputInterface $input) {
         $parameters = new SessionParameters();
         $parameters->apiKey = $input->getOption(Parameter::API_KEY) ?: $this->apiKey;
@@ -33,8 +70,54 @@ class SessionParametersFactory {
         $parameters->cabinClass = $input->getOption(Parameter::CABIN_CLASS);
         $parameters->children = $input->getOption(Parameter::CHILDREN);
         $parameters->infants = $input->getOption(Parameter::INFANTS);
-        $parameters->groupPricing= $input->getOption(Parameter::GROUP_PRICING);
+        $parameters->groupPricing = $input->getOption(Parameter::GROUP_PRICING);
 
         return $parameters;
+    }
+
+    /**
+     * @return array
+     */
+    public function getMaxPrices() {
+        return $this->maxPrices;
+    }
+
+    /**
+     * @param array $configuration
+     * @return SessionParameters
+     */
+    private function createFromArray(array $configuration) {
+        $this->configCache = $configuration;
+
+        $parameters = new SessionParameters();
+        $parameters->apiKey = $this->getValue(Parameter::API_KEY, $this->apiKey);
+        $parameters->originPlace = $this->getValue(Parameter::FROM);
+        $parameters->destinationPlace = $this->getValue(Parameter::TO);
+        $parameters->outboundDate = $this->getValue(Parameter::DEPARTURE_DATE);
+        $parameters->inboundDate = $this->getValue(Parameter::RETURN_DATE);
+        $parameters->locationSchema = $this->getValue(Parameter::LOCATION_SCHEMA, Parameter::DEFAULT_LOCATION_SCHEMA);
+        $parameters->country = $this->getValue(Parameter::COUNTRY);
+        $parameters->currency = $this->getValue(Parameter::CURRENCY);
+        $parameters->locale = $this->getValue(Parameter::LOCALE);
+        $parameters->adults = $this->getValue(Parameter::ADULTS, Parameter::DEFAULT_ADULTS);
+        $parameters->cabinClass = $this->getValue(Parameter::CABIN_CLASS, Parameter::DEFAULT_CABIN_CLASS);
+        $parameters->children = $this->getValue(Parameter::CHILDREN, Parameter::DEFAULT_CHILDREN);
+        $parameters->infants = $this->getValue(Parameter::INFANTS, Parameter::DEFAULT_INFANTS);
+        $parameters->groupPricing = $this->getValue(Parameter::GROUP_PRICING, Parameter::DEFAULT_GROUP_PRICING);
+
+        return $parameters;
+    }
+
+    /**
+     * @param string $parameter
+     * @param mixed $defaultValue
+     * @return mixed
+     */
+    private function getValue($parameter, $defaultValue = null) {
+        if (isset($this->configCache[$parameter])) {
+            return $this->configCache[$parameter];
+        }
+
+        return $defaultValue;
     }
 }
