99ce5dd23a56355cb21c4637117f1fa19dfb41e5
Jeancsil <jeancsil@gmail.com>
2016-09-25 21:02:20 +0200

    Add the default option values into the Parameter entity.

--- src/Entity/Parameter.php
+++ src/Entity/Parameter.php
@@ -6,6 +6,7 @@
 namespace Jeancsil\FlightSpy\Entity;
 
 class Parameter {
+    const FILE = 'file';
     const FROM = 'from';
     const TO = 'to';
     const DEPARTURE_DATE= 'departure';
@@ -13,12 +14,18 @@ class Parameter {
     const MAX_PRICE = 'max-price';
     const API_KEY = 'api-key';
     const LOCATION_SCHEMA = 'location-schema';
+    const DEFAULT_LOCATION_SCHEMA = 'Sky';
     const COUNTRY = 'country';
     const CURRENCY = 'currency';
     const LOCALE = 'locale';
     const ADULTS = 'adults';
+    const DEFAULT_ADULTS = 1;
     const CHILDREN = 'children';
+    const DEFAULT_CHILDREN = 0;
     const INFANTS = 'infants';
+    const DEFAULT_INFANTS = 0;
     const CABIN_CLASS = 'cabin-class';
+    const DEFAULT_CABIN_CLASS = 'Economy';
     const GROUP_PRICING = 'price-per-adult';
+    const DEFAULT_GROUP_PRICING = false;
 }
