0d28795fb06c1bd9747b95fe5702c0de6ae273e7
Jeancsil <jeancsil@gmail.com>
2016-09-27 08:41:03 +0200

    Add PHPUnit support and dummy test to validate travis-ci config.

---
+++ test/Api/DataTransfer/SessionParametersFactoryTest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Jean Silva <jeancsil@gmail.com>
+ * @license MIT
+ */
+namespace Jeancsil\FlightSpy\Api\DataTransfer;
+
+class SessionParametersFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDummy() {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+}
