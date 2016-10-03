48f61576b643f0b02a18056268335f0009bc500d
Jeancsil <jeancsil@gmail.com>
2016-10-03 02:10:27 +0200

    Turn the project much more accessible application using Docker.

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM php
 ENV DEBIAN_FRONTEND=noninteractive
 
 RUN apt-get update \
-    && apt-get install -y vim cron \
+    && apt-get install -y vim cron wget zip unzip \
     && apt-get clean \
     && rm -rf /var/lib/apt/lists/*
 
@@ -11,7 +11,8 @@ ADD . /flight-spy
 ADD docker/crontab /etc/cron.d/flight-spy-cron
 ADD docker/php.ini /usr/local/etc/php/php.ini
 
-RUN chmod 0644 /etc/cron.d/flight-spy-cron && \
+RUN bash /flight-spy/docker/entrypoint.sh && \
+chmod 0644 /etc/cron.d/flight-spy-cron && \
 touch /var/log/cron.log
 
 CMD cron && tail -f /var/log/cron.log
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-EXPECTED_SIGNATURE=$(wget https://composer.github.io/installer.sig -O - -q)
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+set -x
 
-if [ "$EXPECTED_SIGNATURE" = "$ACTUAL_SIGNATURE" ]
-then
-    php composer-setup.php --quiet
-    RESULT=$?
-    rm composer-setup.php
-    exit $RESULT
-else
-    >&2 echo 'ERROR: Invalid installer signature'
-    rm composer-setup.php
-    exit 1
+COMPOSER=`which composer`
+MUST_INSTALL=$?
+
+if [ "$MUST_INSTALL" != 0 ]; then
+    EXPECTED_SIGNATURE=$(wget https://composer.github.io/installer.sig -O - -q)
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+
+    if [ "$EXPECTED_SIGNATURE" = "$ACTUAL_SIGNATURE" ]
+    then
+        php composer-setup.php --install-dir=.
+        rm composer-setup.php
+        COMPOSER="/composer.phar";
+    else
+        >&2 echo 'ERROR: Invalid installer signature'
+        rm composer-setup.php
+        exit 1
+    fi
 fi
 
-composer install --optimize-autoloader --no-suggest --no-interaction --no-dev --prefer-dist
+#adduser --disabled-password --gecos '' r
+#adduser r sudo
+#echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+
+su -c "cd /flight-spy/ && $COMPOSER install --no-plugins --no-scripts --optimize-autoloader --no-suggest --no-interaction --no-dev --prefer-dist"
