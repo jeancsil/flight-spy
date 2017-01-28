7df6d4a1676fd92cf1b8eddc78fc8d7f97912c95
Jeancsil <jeancsil@gmail.com>
2017-01-28 20:56:51 +0100

    Fix build.sh directories

--- bin/build.sh
+++ bin/build.sh
@@ -4,6 +4,6 @@
 set -e
 set -o pipefail
 
-php bin/phpunit --coverage-html ../reports/coverage
+php bin/phpunit --coverage-html reports/coverage
 php bin/phpcs
-php bin/phpmd src html cleancode,codesize,controversial,design,naming,unusedcode --reportfile ../reports/mess_detector.html
+php bin/phpmd src html cleancode,codesize,controversial,design,naming,unusedcode --reportfile reports/mess_detector.html
