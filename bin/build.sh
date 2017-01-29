6ddfacb84530020367734bfda9be7a1f9e8a9bcd
Jeancsil <jeancsil@gmail.com>
2017-01-29 16:16:03 +0100

    Configure PHP CodeSniffer to follow the PSR standards

--- bin/build.sh
+++ bin/build.sh
@@ -5,5 +5,5 @@ set -e
 set -o pipefail
 
 php bin/phpunit --coverage-html reports/coverage
-php bin/phpcs
+php bin/phpcs -p --colors --standard=PSR2 src
 php bin/phpmd src html cleancode,codesize,controversial,design,naming,unusedcode --reportfile reports/mess_detector.html
