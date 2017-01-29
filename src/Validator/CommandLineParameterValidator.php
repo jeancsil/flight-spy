1081996d5ecd8521d0f2bdf1b7e0bd41a7295d2b
Jeancsil <jeancsil@gmail.com>
2017-01-29 16:05:47 +0100

    Remove old parameters. Only supporting now --file.

--- src/Validator/CommandLineParameterValidator.php
+++ src/Validator/CommandLineParameterValidator.php
@@ -6,7 +6,6 @@
 namespace Jeancsil\FlightSpy\Validator;
 
 use Jeancsil\FlightSpy\Command\Entity\Parameter;
-use Symfony\Component\Console\Exception\InvalidOptionException;
 use Symfony\Component\Console\Input\InputInterface;
 
 class CommandLineParameterValidator implements ValidatorInterface
@@ -34,35 +33,14 @@ class CommandLineParameterValidator implements ValidatorInterface
     }
 
     /**
-     * TODO validate all fields
      * @throw ValidationException
      */
     public function validate()
     {
         $configFile = $this->input->getOption(Parameter::FILE);
-        $from = $this->input->getOption(Parameter::FROM);
-        $to = $this->input->getOption(Parameter::TO);
-        $departureDate = $this->input->getOption(Parameter::DEPARTURE_DATE);
-        $returnDate = $this->input->getOption(Parameter::RETURN_DATE);
-        $maxPrice = $this->input->getOption(Parameter::MAX_PRICE);
 
-        if ($configFile && file_exists($configFile)) {
-            return; // TODO validate file content
-            //throw new \InvalidArgumentException(sprintf('File not found: %s', $configFile));
-        }
-
-        $options = [
-            '--from' => $from,
-            '--to' => $to,
-            '--departure' => $departureDate,
-            '--arrival' => $returnDate,
-            '--max-price' => $maxPrice
-        ];
-
-        foreach ($options as $longForm => $option) {
-            if (!$option) {
-                throw new InvalidOptionException("Option $longForm not defined");
-            }
+        if (!$configFile || !file_exists($configFile)) {
+            throw new \InvalidArgumentException(sprintf('File not found: %s', $configFile));
         }
     }
 }
